494ed9675e1e0e21f552bd1ebb2f405eb6f51923
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-10-19 13:27:13 -0400

    built out the login page and used localStorage to save token and push to bubblepage entering the right credentials

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Login from "./components/Login";
+// import ColorList from './components/ColorList';
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Route exact path="/login" component={Login} />
+        {/* <Route path="/color-list" component={ColorList} /> */}
+        <Route exact path="/" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
+  const [form, setForm] = React.useState({
+    username: "",
+    password: ""
+  });
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", form)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err.response);
+        setForm({
+          username: "",
+          password: ""
+        });
+      });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChanges = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={form.username}
+          onChange={handleChanges}
+        />
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChanges}
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
